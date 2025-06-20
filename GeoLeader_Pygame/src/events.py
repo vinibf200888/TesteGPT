077f2a39f948d00de31ccbb37aec9691dbe55fdc
vinibf200888 <vinicius.marques1010@hotmail.com>
2025-06-20 17:14:53 -0300

    Implement calendar system and random events

--- GeoLeader_Pygame/src/events.py
+++ GeoLeader_Pygame/src/events.py
@@ -1,4 +1,12 @@
-"""Modulo events - logica placeholder."""
+"""Gerencia eventos aleatórios do jogo."""
+
+import random
+
+
+def check_monthly_events(game_state, ui):
+    """Dispara eventos aleatórios ao avançar o mês."""
+    if random.random() < 0.1:
+        ui.show_popup("Protestos nas ruas!", on_view=lambda: print("VIEW EVENT"))
 
 
 def update(game_state):
